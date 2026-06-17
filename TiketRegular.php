8336a3819d955a79c41a3ba8b0fb8c341aaaf915
Gery Putra Setiawan <59498820+gerypscs@users.noreply.github.com>
2026-06-17 09:10:21 +0700

    [Tahap 4] Implementasi Pewarisan (Inheritance)

--- TiketRegular.php
+++ TiketRegular.php
@@ -2,25 +2,20 @@
 require_once 'Tiket.php';
 
 class TiketRegular extends Tiket {
-    // Properti tambahan spesifik kelas anak
     private $tipeAudio;
     private $lokasiBaris;
 
-    // Constructor Kelas Anak
     public function __construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket, $tipeAudio, $lokasiBaris) {
-        // Memanggil constructor dari class induk (Tiket)
         parent::__construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket);
         $this->tipeAudio = $tipeAudio;
         $this->lokasiBaris = $lokasiBaris;
     }
 
-    // Implementasi Method Abstrak: Menghitung total harga untuk Regular
+    // TAHAP 5: Overriding hitungTotalHarga sesuai soal
     public function hitungTotalHarga() {
-        // Misal: Regular tidak ada biaya tambahan studio
-        return $this->hargaDasarTiket * $this->jumlah_kursi;
+        return $this->jumlah_kursi * $this->hargaDasarTiket;
     }
 
-    // Implementasi Method Abstrak: Menampilkan fasilitas Regular
     public function tampilkanInfoFasilitas() {
         echo "=== FASILITAS STUDIO REGULAR ===<br>";
         echo "Lokasi Baris: " . $this->lokasiBaris . "<br>";
