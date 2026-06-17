48dfbcde3bdd02d27333011e023dc9bb557ec279
Gery Putra Setiawan <59498820+gerypscs@users.noreply.github.com>
2026-06-17 09:13:02 +0700

    [Tahap 4] Implementasi Pewarisan (Inheritance)

--- TiketIMAX.php
+++ TiketIMAX.php
@@ -2,7 +2,6 @@
 require_once 'Tiket.php';
 
 class TiketIMAX extends Tiket {
-    // Properti tambahan spesifik kelas anak
     private $kacamata3dId;
     private $efekGerakFitur;
 
@@ -12,18 +11,15 @@ class TiketIMAX extends Tiket {
         $this->efekGerakFitur = $efekGerakFitur;
     }
 
-    // Implementasi Method Abstrak: Menghitung total harga untuk IMAX
+    // TAHAP 5: Overriding hitungTotalHarga sesuai soal
     public function hitungTotalHarga() {
-        // Misal: Studio IMAX ada tambahan biaya teknologi sebesar Rp 25.000 per kursi
-        $biayaTambahanIMAX = 25000;
-        return ($this->hargaDasarTiket + $biayaTambahanIMAX) * $this->jumlah_kursi;
+        return ($this->jumlah_kursi * $this->hargaDasarTiket) + 35000;
     }
 
-    // Implementasi Method Abstrak: Menampilkan fasilitas IMAX
     public function tampilkanInfoFasilitas() {
         echo "=== FASILITAS STUDIO IMAX ===<br>";
         echo "Teknologi Layar/Gerak: " . $this->efekGerakFitur . "<br>";
-        echo "ID Kacamata 3D: " . ($this->kacamata3dId ?? "Tidak Menggunakan Kacamata 3D") . "<br>";
+        echo "ID Kacamata 3D: " . ($this->kacamata3dId ?? "Tidak Ada") . "<br>";
         echo "--------------------------------<br>";
     }
 }
